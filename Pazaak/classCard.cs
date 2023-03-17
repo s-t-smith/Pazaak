235f1f8dccc64108ec85724083a2d9f2b7baff62
Sean Smith <sean.smith01@l3harris.com>
2023-03-17 13:58:37 -0500

    Added base classes; created the basic classes that will be used during gameplay, fields and methods upcoming.

---
+++ Pazaak/classCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pazaak
+{
+    internal class Card
+    {
+        // Fields:
+        public int Val { get; protected set; }
+
+        // Methods:
+        protected Card() { }
+        protected Card(int val) { }
+        
+    }
+}
