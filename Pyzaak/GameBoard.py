135ca629dc7d2cd3dc6aaf527c512ef73e9a4090
Sean Smith <sean.smith01@l3harris.com>
2023-05-17 16:25:58 -0500

    Fixed gitignore; tried getting a working window to appear, not going great.

--- Pyzaak/GameBoard.py
+++ Pyzaak/GameBoard.py
@@ -3,12 +3,19 @@ import tkinter
 # Extends the Tk class:
 class GameBoard(tkinter.Tk):
     def __init__(self):
-        super().__init__() # super() apparently to expose methods from Tk to the GameBoard class, lets us use the base class __init__.
+        # super().__init__()
+            # super() apparently to expose methods from Tk to the GameBoard class, lets us use the base class __init__.
         
         # Set configuration options for the root window:
         self.title("Pyzaak")
         
         # Set buttons:
-        self.buttonEndTurn
-        self.buttonStand
-        self.buttonQuit
+        self.buttonEndTurn = tkinter.Button(text="End Turn")
+        self.buttonEndTurn.pack()
+        self.buttonStand = tkinter.Button(text="Stand")
+        self.buttonStand.pack()
+        self.buttonQuit = tkinter.Button(text="Quit")
+        self.buttonQuit.pack()
+        
+    def __main__(self):
+        self.mainloop()
--- Pyzaak/Pyzaak.py
+++ Pyzaak/Pyzaak.py
@@ -2,7 +2,6 @@ import GameBoard
 
 class Pyzaak():
 	
-	
-if __name__ == "__main__":
-	game = GameBoard()
-	game.mainloop()
+    def __name__ == "__main__":
+        game = GameBoard()
+        game.mainloop()
