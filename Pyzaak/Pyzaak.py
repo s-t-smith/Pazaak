3748e8a56edc3f9f70a36a5d1f874ac1391c1c2c
s-t-smith <smith2800@gmail.com>
2023-05-20 18:24:19 -0500

    Added Hand methods; need to iron out the new method.

--- Pyzaak/Pyzaak.py
+++ Pyzaak/Pyzaak.py
@@ -2,7 +2,8 @@
 
 from class_GameBoard import GameBoard
 from class_Deck import Deck
-from class_Card import Card
+# from class_Card import Card
+    # Already imported by Deck
 from class_Hand import Hand
 
 class Pyzaak():
@@ -10,24 +11,40 @@ class Pyzaak():
     def __init__(self):
         
         # Create the table deck:
-        tableDeck = Deck(True)
+        self.tableDeck = Deck(True)
 
         # Initialize scores:
-        playerScore = 0
-        cpuScore = 0
+            # these are for tracking a round's score:
+        self.playerScore = 0
+        self.cpuScore = 0
+                    # these are for tracking the number of rounds won:
+        self.playerRounds = 0
+        self.cpuRounds = 0
 
         # Create player decks:
-        playerDeck = Deck(False)
-        cpuDeck = Deck(False)
+        self.playerDeck = Deck(False)
+        self.cpuDeck = Deck(False)
 
         # Create player hands:
-        playerHand = Hand(playerDeck.draw(4))
-        cpuHand = Hand(cpuDeck.draw(4))
+        self.playerHand = Hand(self.playerDeck.draw(4))
+        self.cpuHand = Hand(self.cpuDeck.draw(4))
 
         # Choose first player turn:
-        playerTurnDraw = tableDeck.peek()
-        cpuTurnDraw = tableDeck.peek()
-        if playerTurnDraw > cpuTurnDraw:
-            playerFirst = True
+        playerTurnDraw = self.tableDeck.peek()
+        cpuTurnDraw = self.tableDeck.peek()
+        if playerTurnDraw >= cpuTurnDraw:
+            self.playerFirst = True
         else:
-            playerFirst = False
+            self.playerFirst = False
+
+    def __str__(self) -> str:
+        outString = f"Player Score: {self.playerScore}\n"
+        outString += f"Player Hand: "
+        for card in self.playerHand.cards:
+            outString = f"{outString} {card.show()}"
+        outString += "\n\n"
+        outString += f"Computer Score: {self.cpuScore}\n"
+        outString += f"Computer Hand: "
+        for card in self.cpuHand.cards:
+            outString = f"{outString} {card.show()}"
+        return outString
--- Pyzaak/class_Hand.py
+++ Pyzaak/class_Hand.py
@@ -1,13 +1,19 @@
 # This is the class to implement the player's hand of cards for a game.
 
+from class_Card import Card
+
 class Hand:
     def __init__(self, drawCards: list):
         self.cards = drawCards
 
     def play(self, card: int) -> int:
         playedVal = self.cards[card].show()
-        self.cards.remove[card]
+        self.cards.remove(self.cards[card])
         return playedVal
+    
+    def add(self, card: Card) -> None:
+        if len(self.cards) < 4:
+            self.cards.append(card)
     
     # Going to use this for some manual testing:
     def __str__(self) -> str:
