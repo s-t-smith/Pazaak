9af7e10f7df5c6241caa36f6fed2e0f5a9dda369
s-t-smith <smith2800@gmail.com>
2023-05-20 10:33:57 -0500

    Started laynig out the game init().

--- Pyzaak/Pyzaak.py
+++ Pyzaak/Pyzaak.py
@@ -3,16 +3,31 @@
 from class_GameBoard import GameBoard
 from class_Deck import Deck
 from class_Card import Card
+from class_Hand import Hand
 
 class Pyzaak():
 	
     def __init__(self):
+        
         # Create the table deck:
         tableDeck = Deck(True)
+
+        # Initialize scores:
+        playerScore = 0
+        cpuScore = 0
 
         # Create player decks:
         playerDeck = Deck(False)
         cpuDeck = Deck(False)
 
-        # TODO: create player hands
-        # TODO: choose first turn
+        # Create player hands:
+        playerHand = Hand(playerDeck.draw(4))
+        cpuHand = Hand(cpuDeck.draw(4))
+
+        # Choose first player turn:
+        playerTurnDraw = tableDeck.peek()
+        cpuTurnDraw = tableDeck.peek()
+        if playerTurnDraw > cpuTurnDraw:
+            playerFirst = True
+        else:
+            playerFirst = False
