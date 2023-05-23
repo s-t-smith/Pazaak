2b2229526929a4ae66f1ad72eae0dfa87feef3c4
Sean Smith <sean.smith01@l3harris.com>
2023-05-23 14:36:10 -0500

    Basic frames created, basic buttons working.

--- Pyzaak/class_GameBoard.py
+++ Pyzaak/class_GameBoard.py
@@ -8,24 +8,31 @@ class GameBoard(tkinter.Tk):
         super().__init__()
             # super() apparently to expose methods from Tk to the GameBoard class, lets us use the base class __init__.
         
-        # Set configuration options for the root window:
+        # Set configuration options for the root window (self):
         self.title("Pyzaak")
 
-        # TODO: Create frames:
-        # Should have a start page with "Start" and "Quit"
-        # Next should be the actual game page.
-        self.startFrame = tkinter.Frame(self)
-        self.gameFrame = tkinter.Frame(self)
-        
-        # TODO: Create basic buttons.
-        self.btn_Start = tkinter.Button(self.startFrame, text="Start", command=self.gameFrame.tkraise())
+        # Create Start frame:
+        self.startFrame = tkinter.Frame(self)   # Keeps the app from launching straight into a game.
+        self.startFrame.title = tkinter.Label(self.startFrame, text="Pyzaak")
+        self.startFrame.title.pack()
+        self.btn_Start = tkinter.Button(self.startFrame, text="Play", command=self.gameStart)
         self.btn_Start.pack()
-        self.btn_Quit = tkinter.Button(self.startFrame, text="Quit", command=self.quit())
-        self.btn_Quit.pack()
+        self.btn_startQuit = tkinter.Button(self.startFrame, text="Quit", command=self.destroy)
+        self.btn_startQuit.pack()
         self.startFrame.pack()
-        self.gameFrame.pack()
         self.startFrame.tkraise()
         
+        # TODO: Create Game frame:
+        self.gameFrame = tkinter.Frame(self)    # Where the actual game will happen.
+        self.gameFrame.title = tkinter.Label(self.gameFrame, text="Pyzaak")
+        self.gameFrame.title.pack()
+        self.btn_gameQuit = tkinter.Button(self.gameFrame, text="Quit", command=self.destroy)
+        self.btn_gameQuit.pack()
         
+    def gameStart(self):
+        self.startFrame.destroy()
+        self.gameFrame.pack()
+        self.gameFrame.tkraise()
+    
     def __main__(self):
         self.mainloop()
