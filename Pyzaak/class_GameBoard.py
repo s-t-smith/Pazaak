9ba206f8ab10fe7daea073aea8e2547672fad92b
Sean Smith <sean.smith01@l3harris.com>
2023-06-29 18:42:51 -0500

    Added more to the player frame.

--- Pyzaak/class_GameBoard.py
+++ Pyzaak/class_GameBoard.py
@@ -50,13 +50,27 @@ class GameBoard(tkinter.Tk):
         
         # TODO: Create player frame:
         ### Player frame will include;
+        self.playerFrame = tkinter.Frame(self.gameFrame)
+        self.playerFrame.title = tkinter.Label(self.playerFrame, text="Player")
             # Round counters
             # Score
+        self.scoreBanner = tkinter.PanedWindow(self.playerFrame)
+        self.playerRoundCounterA = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.playerRoundCounterA.pack(side="left")
+        self.playerRoundCounterB = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.playerRoundCounterB.pack(side="left")
+        self.playerRoundCounterC = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.playerRoundCounterC.pack(side="left")
+        self.playerCurrentScore = tkinter.Label(self.scoreBanner, text="0")
+        self.playerCurrentScore.pack(side="right", padx=10)
+        self.scoreBanner.grid(row=0, column=0)
             # Card grid (play area)
             # Hand grid (buttons)
             # End Turn button
             # Stand button
             # Quit button
+        self.playerFrame.pack()
+        
 
         # TODO: Create CPU frame:
         ### CPU frame will include;
