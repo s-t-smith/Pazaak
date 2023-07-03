ceee4398fad5afda9fd21b0d2cbedee16afbf869
Sean Smith <sean.smith01@l3harris.com>
2023-07-03 11:37:49 -0500

    Test run of the board returns an error, need to fix that.

--- Pyzaak/class_GameBoard.py
+++ Pyzaak/class_GameBoard.py
@@ -51,7 +51,7 @@ class GameBoard(tkinter.Tk):
         self.cpuSide = BoardSide(self.gameFrame, playerName="CPU")
         self.cpuSide.grid(row=1, column=2)
 
-        self.btn_gameQuit = tkinter.Button(self.gameFrame, text="Quit", command=self.__init__())
+        self.btn_gameQuit = tkinter.Button(self.gameFrame, text="Quit", command=self.__init__)
         self.btn_gameQuit.grid(row=2, column=1)
 
         # Swap the opening frame for the game frame.
@@ -76,26 +76,39 @@ class GameBoard(tkinter.Tk):
     def __main__(self):
         self.mainloop()
 
-class BoardSide(tkinter.Frame, playerName):
-    def __init__(self):
+class BoardSide(tkinter.Frame):
+    def __init__(self, playerName):
         super().__init__()
-        # TODO: Create player frame:
         # Player frame will include;
         self.title = tkinter.Label(self, text=playerName)
             # Round counters
             # Score
         self.scoreBanner = tkinter.PanedWindow(self)
-        self.playerRoundCounterA = tkinter.Checkbutton(self.scoreBanner, state="disabled")
-        self.playerRoundCounterA.pack(side="left")
-        self.playerRoundCounterB = tkinter.Checkbutton(self.scoreBanner, state="disabled")
-        self.playerRoundCounterB.pack(side="left")
-        self.playerRoundCounterC = tkinter.Checkbutton(self.scoreBanner, state="disabled")
-        self.playerRoundCounterC.pack(side="left")
-        self.playerCurrentScore = tkinter.Label(self.scoreBanner, text="0")
-        self.playerCurrentScore.pack(side="right", padx=10)
+        self.roundCounterA = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.roundCounterA.pack(side="left")
+        self.roundCounterB = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.roundCounterB.pack(side="left")
+        self.roundCounterC = tkinter.Checkbutton(self.scoreBanner, state="disabled")
+        self.roundCounterC.pack(side="left")
+        self.currentScore = tkinter.Label(self.scoreBanner, text="0")
+        self.currentScore.pack(side="right", padx=10)
         self.scoreBanner.grid(row=0, column=0)
             # Card grid (play area)
+        self.cardGrid = tkinter.PanedWindow(self)
+        
+        self.cardGrid.grid(row=1, column=0)
             # Hand grid (buttons)
+        self.handGrid = tkinter.PanedWindow(self)
+
+        self.handGrid.grid(row=2, column=0)
+
+        ### If the frame is for the human player:
             # End Turn button
             # Stand button
-        
+        if playerName!="CPU":
+            self.buttonGrid = tkinter.PanedWindow(self)
+            self.buttonGrid.btn_ET = tkinter.Button(self.buttonGrid, text="End Turn")
+            self.buttonGrid.btn_ET.pack()
+            self.buttonGrid.btn_ST = tkinter.Button(self.buttonGrid, text="Stand")
+            self.buttonGrid.btn_ST.pack()
+            self.buttonGrid.grid(row=3, column=0)
