4a32ad461eb0c54450715fb6847271cab66e7729
s-t-smith <smith2800@gmail.com>
2023-07-22 19:14:13 -0500

    Refactoring Pyzaak class as statemachine.

--- Pyzaak/class_Pyzaak.py
+++ Pyzaak/class_Pyzaak.py
@@ -1,41 +1,46 @@
 # This file will be the overall main for the game.
 
 from class_Deck import Deck
+from statemachine import StateMachine, State
 
-class Pyzaak():
-	
+class Pyzaak(StateMachine):
+	UserTurn = State()
+    ComTurn = State()
+    GameOver = State()
+
     def __init__(self):
+        super.__init__()
 
         # Initialize scores:
             # these are for tracking a round's score:
-        self.playerScore = 0
-        self.cpuScore = 0
+        self.userScore = 0
+        self.comScore = 0
                     # these are for tracking the number of rounds won:
-        self.playerRounds = 0
-        self.cpuRounds = 0
+        self.userRounds = 0
+        self.comRounds = 0
 
     # TODO: figure out how this game object will interact with the game board.
         ### probably need a lot of pass-by-ref.
     # TODO: add more methods for implimenting the game rules.
         ### Maybe create a state machine?
 
-    def addPlayerScore(self, score: int) -> None:
-        self.playerScore += score
+    def addUserScore(self, score: int) -> None:
+        self.userScore += score
 
-    def playerRoundWin(self) -> None:
-        self.playerRounds += 1
+    def userRoundWin(self) -> None:
+        self.userRounds += 1
     
-    def addCPUScore(self, score: int) -> None:
-        self.cpuScore += score
+    def addComScore(self, score: int) -> None:
+        self.comScore += score
 
-    def cpuRoudnWin(self) -> None:
-        self.cpuRounds += 1
+    def comRoundWin(self) -> None:
+        self.comRounds += 1
 
     def pickFirstTurn(tableDeck: Deck) -> bool:
     # Choose first player turn:
-        playerTurnDraw = tableDeck.peek()
-        cpuTurnDraw = tableDeck.peek()
-        if playerTurnDraw >= cpuTurnDraw:
+        userTurnDraw = tableDeck.peek()
+        comTurnDraw = tableDeck.peek()
+        if userTurnDraw >= comTurnDraw:
             return True
         else:
             return False
