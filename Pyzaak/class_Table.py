f9eb5c84af3408f6f3cfec1e5c74e16df54f4733
s-t-smith <smith2800@gmail.com>
2023-08-11 19:53:49 -0500

    Added a class to handle the play area; still working the unit test for Pyzaak.

---
+++ Pyzaak/class_Table.py
@@ -0,0 +1,6 @@
+# This file will serve as the play area for each opponent.
+# Each player gets 15 slots to use during a game. When the slots are full, the game is over.
+
+from class_Card import Card
+
+class Table:
